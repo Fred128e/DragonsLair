0edc09df35d3268c8e60561b4557e95d4448c461
Frederik Nørager Horne Nielsen <fred128e@edu.eal.dk>
2017-10-27 15:00:07 +0200

    For loop added to ShowScore in "Controller.cs"

--- DragonsLair_1/Controller.cs
+++ DragonsLair_1/Controller.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DragonsLair_1
 {
@@ -18,8 +19,11 @@ namespace DragonsLair_1
                 foreach (Team winningTeam in winningTeams)
                 {
                     Console.WriteLine(winningTeam.Name);
+                    
                 }
+                
             }
+
 
 
             /*
